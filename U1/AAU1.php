33f9d2b79c5114ec0ba6a620192113a84bc4c74c
RobertoAnguloDeveloper <rangulotl@outlook.com>
2022-05-16 22:01:07 -0500

    CSS 3

--- U1/AAU1.php
+++ U1/AAU1.php
@@ -48,27 +48,27 @@
                 <table class="tabla">
 
                     <!-- Crea una tabla com los datos del formulario  -->
-                    <?php
-                    while (key($_POST) != null) {
-                        echo "<th><b>" . strtoupper(key($_POST)) . "</b></th>";
-                        next($_POST);
-                    }
+    <?php
+    while (key($_POST) != null) {
+        echo "<th><b>" . strtoupper(key($_POST)) . "</b></th>";
+        next($_POST);
+    }
 
-                    //Rebobina el puntero al principio del array
-                    reset($_POST);
+    //Rebobina el puntero al principio del array
+    reset($_POST);
 
-                    echo "<tr>";
+    echo "<tr>";
 
-                    while (key($_POST) != null) {
-                        echo "<td>" . current($_POST) . "</td>";
-                        next($_POST);
-                    }
+    while (key($_POST) != null) {
+        echo "<td>" . current($_POST) . "</td>";
+        next($_POST);
+    }
 
-                    echo "</tr>";
-                    ?>
-                </table>
-            </td>
-        </tr>
+    echo "</tr>";
+    ?>
+    </table>
+    </td>
+    </tr>
     </table>
     </center>
     <br>
