b13f1c35832167ea15d24e6c133b76a5d24e8122
RobertoAnguloDeveloper <rangulotl@outlook.com>
2022-06-07 12:45:42 -0500

    AAU1 terminada

--- orm/conifg.php
+++ orm/conifg.php
@@ -1,9 +1,10 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/../config.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/desarrolloweb/orm/php-activerecord/ActiveRecord.php';
 
 ActiveRecord\Config::initialize(function($cfg)
 {
-   $cfg->set_model_directory('/path/to/your/model_directory');
+
+   $cfg->set_model_directory('./model');
    $cfg->set_connections(
      array(
        'development' => 'mysql://username:password@localhost/development_database_name',
